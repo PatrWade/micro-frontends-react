804bf564b45e1417b1f50b670f5bc949b17f3382
Patrick Wade <patrick.wade@accenture.com>
2023-12-12 13:56:16 +0100

    feat(all): add communication between products and checkout

--- packages/checkout/src/components/navigation-cart/index.jsx
+++ packages/checkout/src/components/navigation-cart/index.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./styles";
 import { CartIcon } from "../icons/cart-icon";
 
 export const NavigationCart = () => {
+  const [cartProducts, setCartProducts] = useState([]);
+
+  const listener = (event) => {
+    setCartProducts((currentProducts) =>
+      currentProducts.concat(event.detail.productId)
+    );
+  };
+
+  useEffect(() => {
+    window.addEventListener("ADD_TO_CART", listener);
+
+    return () => {
+      window.removeEventListener("ADD_TO_CART", listener);
+    };
+  }, []);
+
   return (
     <span className={`${styles.navigationCart}`}>
       <CartIcon />
-      <span className={`${styles.itemCount}`}>3</span>
+      <span className={`${styles.itemCount}`}>{cartProducts.length}</span>
     </span>
   );
 };
--- packages/products/src/components/product/index.jsx
+++ packages/products/src/components/product/index.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useId } from "react";
 import styles from "./styles";
 
 export const Product = () => {
+  const productId = useId();
+
+  const addToCartEvent = () => {
+    const addEvent = new CustomEvent("ADD_TO_CART", {
+      detail: { productId },
+    });
+    window.dispatchEvent(addEvent);
+  };
+
   return (
     <article className={`${styles.article}`}>
       <div className={`${styles.skeleton} ${styles.image}`} />
       <div className={`${styles.descriptionContainer}`}>
         <div className={`${styles.skeleton} ${styles.description}`} />
         <div className={`${styles.skeleton} ${styles.description}`} />
-        <div className={`${styles.skeleton} ${styles.description}`} />
+        <button
+          className={`${styles.skeleton} ${styles.description}`}
+          onClick={addToCartEvent}
+        >
+          add product
+        </button>
       </div>
     </article>
   );
